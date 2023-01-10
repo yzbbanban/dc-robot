d87220c3866fd2a237ea24ad49557a4a0402ddfc
踩踩多来梦 <357351735@qq.com>
2023-01-10 13:02:53 +0800

    first commit

---
+++ src/main/java/com/yzb/discordrobot/DiscordrobotApplication.java
@@ -0,0 +1,13 @@
+package com.yzb.discordrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordrobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordrobotApplication.class, args);
+    }
+
+}
