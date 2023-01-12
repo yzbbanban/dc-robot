b715ec6842ee8bb4929720d8ddc54285c41fc212
踩踩多来梦 <357351735@qq.com>
2023-01-13 00:05:07 +0800

    README.md

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ The following guides illustrate how to use some features concretely:
 
 https://jda.wiki/introduction/events/
 https://blog.csdn.net/qq_38490457/article/details/125250135
+
+说明：
+
+1. 配置好机器人，替换 your token 即可
+2. 启动会等一会，自动加入到等待队列中机器人需要在线等待一段时间
+   出现这个，请不要关闭，等待即可【Failed to establish connection for a node, appending to queue】
--- src/main/java/com/yzb/discordrobot/DiscordrobotApplication.java
+++ src/main/java/com/yzb/discordrobot/DiscordrobotApplication.java
@@ -2,6 +2,7 @@ package com.yzb.discordrobot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,11 +12,14 @@ public class DiscordrobotApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscordrobotApplication.class, args);
         try {
-            String token = "--your--token--";
+            String token = "your token";
             // Note: It is important to register your ReadyListener before building
             JDA jda = JDABuilder.createDefault(token)
-                    .addEventListeners(new MessageEvent())
+//                    .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                     .build();
+
+            jda.addEventListener(new MessageEvent());
+
 
             // optionally block until JDA is ready
             jda.awaitReady();
